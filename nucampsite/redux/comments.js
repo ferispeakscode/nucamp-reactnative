907fa57839b7e944bd06e2eabe41ddd31ade1c08
Felicia Tchen <fseri@feriXIA.fios-router.home>
2020-09-19 14:12:47 -0400

    Post comments to campsites via modal form (Week 2 Workshop Assignment)

--- nucampsite/redux/comments.js
+++ nucampsite/redux/comments.js
@@ -5,6 +5,16 @@ export const comments = (state = { errMess: null, comments: []}, action) => {
         case ActionTypes.ADD_COMMENTS:
             return {...state, errMess: null, comments: action.payload};
 
+        case ActionTypes.ADD_COMMENT:
+            //add id to comment
+            //working in the reducer - must use "state"
+            let newComment = {...action.payload, id: state.comments.length}
+            //state is immutable in react - create a copy of the comments array to make changes
+            let newComments = state.comments.slice();
+            //add new comment to array of existing comments
+            newComments.push(newComment);
+            return {...state, comments: newComments};
+
         case ActionTypes.COMMENTS_FAILED:
             return {...state, errMess: action.payload};
 
